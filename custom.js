917ae54960ba2454ede0b7ee7afe17933f656eaf
anjitzero <45827003+anjitzero@users.noreply.github.com>
2019-10-18 23:42:02 +0545

    Add files via upload

---
+++ custom.js
@@ -0,0 +1,30 @@
+
+var x = document.querySelectorAll(".guitar1"); 
+var a;
+
+function playAudio(a) {
+	for(var i=0;i<=3;i++){
+		x[i].pause();
+		console.log("pause");
+	};
+	x[a].play();
+	console.log("play");
+} 
+
+function pauseAudio() { 
+  x.pause(); 
+}
+var soundBox= document.querySelector(".sound-box");
+var soundBox1= document.querySelectorAll(".soundbox-1");
+soundBox1[0].addEventListener('click',function(){
+	playAudio(0);
+});
+soundBox1[1].addEventListener('click',function(){
+	playAudio(1);
+});
+soundBox1[2].addEventListener('click',function(){
+	playAudio(2);
+});
+soundBox1[3].addEventListener('click',function(){
+	playAudio(3);
+});
